2073641797c8de20fa246d9bc65d77a6dd546825
Vuong Vu <hoangvuong.v99@gmail.com>
2024-04-01 16:14:46 +0200

    Refactor Button component to prevent space key event propagation

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import styles from "./style.module.css";
 
-interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   icon: React.ReactNode;
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({
-  icon,
-  onClick,
-  ...restProps
-}) => {
+const Button: React.FC<ButtonProps> = ({ icon, onClick, ...restProps }) => {
   return (
     <button
       className={styles.button}
       onClick={onClick}
       onKeyDown={(event) => {
         if (event.key === " ") {
-          console.log("Space key pressed");
-          event.preventDefault();
+          event.stopPropagation();
         }
       }}
       {...restProps}
