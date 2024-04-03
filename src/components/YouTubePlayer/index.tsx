2e4cbe21387b18ceb164e124cc029ffd78bc98e2
Vuong Vu <hoangvuong.v99@gmail.com>
2024-04-03 16:29:24 +0200

    Refactor YouTubePlayer initialization

--- src/components/YouTubePlayer/index.tsx
+++ src/components/YouTubePlayer/index.tsx
@@ -21,22 +21,25 @@ const initializePlayer = (
     ref.current.destroy();
   }
 
-  ref.current = new window.YT.Player("player", {
-    height: "auto",
-    width: "100%",
-    videoId,
-    playerVars: {
-      fs: 0, // hide fullscreen button by default
-      playsinline: 1, // make the player not go fullscreen when playing on ios
-      modestbranding: 1, // hide youtube logo by default - we say 'powered by youtube'
-      controls: 0, // hide controls by default
-      showinfo: 0, // hide video title by default
-      iv_load_policy: 3, // hide annotations by default
-      rel: 0,
-    },
-    events: {
-      onReady: cb,
-    },
+  // @ts-expect-error - property exists
+  window.YT.ready(function () {
+    ref.current = new window.YT.Player("player", {
+      height: "auto",
+      width: "100%",
+      videoId,
+      playerVars: {
+        fs: 0, // hide fullscreen button by default
+        playsinline: 1, // make the player not go fullscreen when playing on ios
+        modestbranding: 1, // hide youtube logo by default - we say 'powered by youtube'
+        controls: 0, // hide controls by default
+        showinfo: 0, // hide video title by default
+        iv_load_policy: 3, // hide annotations by default
+        rel: 0,
+      },
+      events: {
+        onReady: cb,
+      },
+    });
   });
 };
 
