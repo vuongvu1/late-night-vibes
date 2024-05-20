7b9243cc09dc44ad01ed5153beaf1385215a42d9
Vuong Vu <hoangvuong.v99@gmail.com>
2024-05-20 15:51:06 +0200

    refactor: Remove TooltipOptions interface from Tooltip component

--- src/components/Tooltip/type.ts
+++
@@ -1,8 +0,0 @@
-import type { Placement } from "@floating-ui/react";
-
-export interface TooltipOptions {
-  initialOpen?: boolean;
-  placement?: Placement;
-  open?: boolean;
-  onOpenChange?: (open: boolean) => void;
-}
