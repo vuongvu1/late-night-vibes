4b2a14d63d935c3a8d2d457338ebffba2d3d625e
Vuong Vu <hoangvuong.v99@gmail.com>
2024-06-01 17:16:42 +0200

    chore: use random GIF

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,11 +1,27 @@
 import styles from "./style.module.css";
+import night1Gif from "../../assets/gifs/night-1.gif";
 import night2Gif from "../../assets/gifs/night-2.gif";
+import night3Gif from "../../assets/gifs/night-3.gif";
+
+const getRandomGif = () => {
+  const randomGif = Math.floor(Math.random() * 3) + 1;
+
+  if (randomGif === 1) {
+    return night1Gif;
+  } else if (randomGif === 2) {
+    return night2Gif;
+  } else {
+    return night3Gif;
+  }
+};
 
 function Background() {
+  const gif = getRandomGif();
+
   return (
     <div className={styles.container}>
-      <img src={night2Gif} alt="foreground" className={styles.foreground}></img>
-      <img src={night2Gif} alt="background" className={styles.background}></img>
+      <img src={gif} alt="foreground" className={styles.foreground}></img>
+      <img src={gif} alt="background" className={styles.background}></img>
     </div>
   );
 }
