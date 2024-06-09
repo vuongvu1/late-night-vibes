5791b772ffc041b162117047479047798a024fd9
Vuong Vu <hoangvuong.v99@gmail.com>
2024-06-09 17:36:51 +0200

    refactor: Remove unused GIF source

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -3,12 +3,11 @@ import src1 from "../../assets/gifs/1.gif";
 import src2 from "../../assets/gifs/2.gif";
 import src3 from "../../assets/gifs/3.gif";
 import src4 from "../../assets/gifs/4.gif";
-import src5 from "../../assets/gifs/5.gif";
 import src6 from "../../assets/gifs/6.webp";
 import src7 from "../../assets/gifs/7.webp";
 import src8 from "../../assets/gifs/8.webp";
 
-const GIFS = [src1, src2, src3, src4, src5, src6, src7, src8];
+const GIFS = [src1, src2, src3, src4, src6, src7, src8];
 
 const getRandomGif = () => {
   const randomGif = Math.floor(Math.random() * GIFS.length) + 1;
