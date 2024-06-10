aa308e2ff98255cb9006ca4780a5c220b624a478
Vuong Vu <hoangvuong.v99@gmail.com>
2024-06-10 11:08:26 +0200

    refactor: Update Background component to use lazy loading for images

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,26 +1,26 @@
+import { useEffect, useState } from "react";
 import styles from "./style.module.css";
-import src1 from "../../assets/gifs/1.gif";
-import src2 from "../../assets/gifs/2.gif";
-import src3 from "../../assets/gifs/3.gif";
-import src4 from "../../assets/gifs/4.gif";
-import src6 from "../../assets/gifs/6.webp";
-import src7 from "../../assets/gifs/7.webp";
-import src8 from "../../assets/gifs/8.webp";
 
-const GIFS = [src1, src2, src3, src4, src6, src7, src8];
-
-const getRandomGif = () => {
-  const randomGif = Math.floor(Math.random() * GIFS.length) + 1;
-  return GIFS[randomGif - 1];
-};
+// @see https://vitejs.dev/guide/features#glob-import
+const modules = import.meta.glob(
+  ["../../assets/gifs/*.gif", "../../assets/gifs/*.webp"],
+  { import: "default" }
+);
+const GIFS = Object.keys(modules).map((path) => modules[path]);
 
 function Background() {
-  const gif = getRandomGif();
+  const [imageSrc, setImageSrc] = useState<string>();
+
+  useEffect(() => {
+    GIFS[Math.floor(Math.random() * GIFS.length)]().then((image) => {
+      setImageSrc(image as string);
+    });
+  }, []);
 
   return (
     <div className={styles.container}>
-      <img src={gif} alt="foreground" className={styles.foreground}></img>
-      <img src={gif} alt="background" className={styles.background}></img>
+      <img src={imageSrc} alt="foreground" className={styles.foreground}></img>
+      <img src={imageSrc} alt="background" className={styles.background}></img>
     </div>
   );
 }
