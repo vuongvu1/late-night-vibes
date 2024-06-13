a269361af3658259ee355b6980ef93c01b59f47b
Vuong Vu <hoangvuong.v99@gmail.com>
2024-06-13 11:32:10 +0200

    Update SVG icons width to 24 for better visibility

--- src/assets/icons/Next.tsx
+++ src/assets/icons/Next.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={20} fill="none" viewBox="0 0 9 9" {...props}>
+  <svg width={24} fill="none" viewBox="0 0 9 9" {...props}>
     <path
       fill="#fff"
       d="M2 1h1v1h1v1h1V1h1v1h1v1h1v1h1v1H8v1H7v1H6v1H5V6H4v1H3v1H2V1Z"
--- src/assets/icons/Play.tsx
+++ src/assets/icons/Play.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
-  <svg width={20} fill="none" viewBox="0 0 9 9" {...props}>
+  <svg width={24} fill="none" viewBox="0 0 9 9" {...props}>
     <path fill="#fff" d="M4 1H3v7h1V7h1V6h1V5h1V4H6V3H5V2H4V1Z" />
   </svg>
 );
